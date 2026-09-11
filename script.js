b7f7833d2d0b6989bdb0ffdf31fca96e1c289235
Sanjana Nikam <sanjananikam4103@gmail.com>
2026-09-11 15:30:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
-let count = 0;
+//funtion to fetch data from server
 
-const countDisplay = document.getElementById("count");
-const incBtn = document.getElementById("incBtn");
-const decBtn = document.getElementById("decBtn");
-const resetBtn = document.getElementById("resetBtn");
+// async function githubUserProfile(username) {
 
-function updateUI() {
-  countDisplay.textContent = count;
+//     //logic
+
+//     const response = await fetch(`https://api.github.com/users/${username}`);
+
+//     const finaldata = await response.json()
+
+//     console.log(finaldata);
+
+// }
+//     githubUserProfile("sanjananikam1592");
+
+
+
+// promise with .then and .catch
+
+// function githubUserProfile(username) {
+//     fetch(`https://api.github.com/users/${username}`)
+//     .then((response) => response.json(), //json form
+// )
+//     .then((finaldata) => console.log(finaldata))
+//     .catch((error) => {
+//         console.log("Error while fetching data", error);
+//     });
+// }
+
+// githubUserProfile("sanjananikam1592");
+
+
+//assesment
+//funtion to fetch data from server
+//  async function typicode() {
+
+// //     //logic
+
+//     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+
+//     const finaldata = await response.json()
+
+//     console.log(finaldata);
+
+//  }
+//     typicode();
+
+
+
+
+
+
+// promise with .then and .catch
+function typicode() {
+    fetch(`https://jsonplaceholder.typicode.com/posts/`)
+    .then((response) => response.json(), //json form
+)
+    .then((finaldata) => console.log(finaldata))
+    .catch((error) => {
+        console.log("Error while fetching data", error);
+    });
 }
 
-incBtn.addEventListener("click", () => {
-  count++;
-  updateUI();
-});
-
-decBtn.addEventListener("click", () => {
-  count--;
-  updateUI();
-});
-
-resetBtn.addEventListener("click", () => {
-  count = 0;
-  updateUI();
-});
+typicode();
